2d1547836d8831141e8b0e6ccb019ebba2261c28
tpo <tpo@g.com>
2024-12-02 22:37:41 -0500

    test

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+/*
+
 reportWebVitals();
 
 
@@ -31,3 +34,5 @@ Sentry.init({
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
+
+*/
