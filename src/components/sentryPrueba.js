241784e062fb8a46c5194927a212df934d86aa3c
tpo <tpo@g.com>
2024-12-02 21:18:17 -0500

    test

--- src/components/sentryPrueba.js
+++ src/components/sentryPrueba.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const SentryPrueba = () => {
+    /*
     const handleClick = () => {
         console.log("¡Botón presionado desde MiComponente!");
     };
     return <button onClick={() => {throw new Error("Un error a proposito");}}>Un error a proposito 😈</button>;
+    */
+    return ""
 };
 
 export default SentryPrueba;
