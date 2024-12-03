9b2057c65f06f288d658153579d94b2c2e4dba2d
tpo <tpo@g.com>
2024-12-02 22:12:45 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+afterEach(() => {
+  jest.clearAllMocks();
+  jest.resetModules();
+});
+
+
 test('renders learn Jenkins link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
